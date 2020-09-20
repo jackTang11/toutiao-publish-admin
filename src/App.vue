5e67fb18333fa9dc6928957035290ca4604075a9
jack <461897266@qq.com>
2020-09-20 23:47:15 +0800

    添加登录页面

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-    <h1>头条内容发布系统</h1>
     <router-view/>
   </div>
 </template>
 
 <script>
 
-export default {
-    name: 'APP'
-}
+  export default {
+    name: 'APP',
+    data () {
+      return {
+      }
+    },
+  }
 </script>
 
 <style lang="less">
